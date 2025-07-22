d0dba4b294b3d669e6fc2a1026a156a07bc76436
Shourya Pandey <163396528+GodricsHeir@users.noreply.github.com>
2025-07-22 21:41:58 +0530

    Add files via upload

---
+++ LongestSubString.py
@@ -0,0 +1,31 @@
+def checkPalindrome(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+
+subColletion = []
+
+def largestInList(subCollection):
+    if not subCollection:
+        return ""
+    largest = subCollection[0]
+    for item in subCollection:
+        if len(item) > len(largest):
+            largest = item
+    return largest
+
+def longestPalindrome(s):
+    limit = len(s)
+    while limit > 0:
+        limit-=1
+        for i in range(len(s) - limit):
+            substring = s[i:i + limit + 1]
+            if checkPalindrome(substring):
+                subColletion.append(substring)
+    if len(largestInList(subColletion))>1:
+        return largestInList(subColletion)
+    else:
+        return "No Palindrome Found"
+    
+print(longestPalindrome("palindrome")) 
